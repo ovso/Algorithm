e51f2c15146c9ccfcc0c432154bc2362e3aa14d9
jaeho <ovsoce@gmail.com>
2020-06-08 22:41:26 +0900

    순열 포함 여부

---
+++ app/src/test/java/io/github/ovso/algorithm/순열.kt
@@ -0,0 +1,22 @@
+@file:Suppress("NonAsciiCharacters", "ClassName", "TestFunctionName")
+
+package io.github.ovso.algorithm
+
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class 순열 {
+    @Test
+    fun 순열을포함하는지여부() {
+        val (s1, s2) = Pair("ab", "eidbaooo")
+        println(solution(s1, s2))
+    }
+
+    private fun solution(s1: String, s2: String): Boolean {
+        return s2.contains(s1) || s2.contains(s1.reversed())
+    }
+}
