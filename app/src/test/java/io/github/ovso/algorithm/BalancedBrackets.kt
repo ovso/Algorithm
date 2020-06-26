b7880e35ddc10501a9c920c90fe84b10f38140c4
jaeho <ovsoce@gmail.com>
2020-06-27 06:42:40 +0900

    Implement palindrome

--- app/src/test/java/io/github/ovso/algorithm/BalancedBrackets.kt
+++ app/src/test/java/io/github/ovso/algorithm/BalancedBrackets.kt
@@ -8,13 +8,23 @@ class BalancedBrackets {
         assert(solution1("aabbaa"))
         assert(solution1("abaabbaaba"))
         assert(solution2("abaabbaaba"))
+        assert(solution2("cccccccccc"))
     }
 
     private fun solution1(s: String): Boolean {
         return s.reversed() == s
     }
 
-    private fun solution2(s: String): Boolean {
+    private fun solution2(palindrome: String): Boolean {
+        val lastIndex = palindrome.lastIndex
+        val untilAt = palindrome.count() / 2
+        for (startIndex in 0 until untilAt) {
+            val endIndex = lastIndex - startIndex
+            println("${palindrome[startIndex]} - ${palindrome[endIndex]} : $startIndex - $endIndex")
+            if (palindrome[startIndex] != palindrome[endIndex]) {
+                return false
+            }
+        }
         return true
     }
 
