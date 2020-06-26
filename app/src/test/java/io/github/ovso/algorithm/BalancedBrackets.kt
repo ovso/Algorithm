4a38fa784a367c0bcde8aafe1d75a0d3290e1750
jaeho <ovsoce@gmail.com>
2020-06-27 00:34:53 +0900

    WIP - BalancedBrackets.kt

---
+++ app/src/test/java/io/github/ovso/algorithm/BalancedBrackets.kt
@@ -0,0 +1,21 @@
+package io.github.ovso.algorithm
+
+import org.junit.Test
+
+class BalancedBrackets {
+    @Test
+    fun main() {
+        assert(solution1("aabbaa"))
+        assert(solution1("abaabbaaba"))
+        assert(solution2("abaabbaaba"))
+    }
+
+    private fun solution1(s: String): Boolean {
+        return s.reversed() == s
+    }
+
+    private fun solution2(s: String): Boolean {
+        return true
+    }
+
+}
